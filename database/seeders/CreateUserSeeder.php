560c681d76099215d86c9821a4342a0170011da1
tikamaisarah <tikamaisarah43483@gmail.com>
2021-09-25 14:02:51 +0700

    Initial commit

---
+++ database/seeders/CreateUserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class CreateUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            [
+                'name' => 'Admin',
+                'email' => 'admin@admin.com',
+                'password' => bcrypt('123456'),
+                'level' => 'admin'
+            ],
+        ];
+        foreach ($users as $key => $value) {
+            User::create($value);
+        }
+    }
+}
