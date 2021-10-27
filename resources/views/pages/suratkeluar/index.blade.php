971af095a9e17a8ab8a07d684fab1fbb3e95578f
unknown <ayuf267@gmail.com>
2021-10-27 15:31:26 +0700

    add surat keluar

--- resources/views/pages/suratkeluar/index.blade.php
+++ resources/views/pages/suratkeluar/index.blade.php
@@ -179,6 +179,36 @@
             <section class="content">
                 <div class="container-fluid">
                     <!-- Small boxes (Stat box) -->
+        
+                    <div class="card">
+                        <div class="card-header">
+                            Surat keluar
+                        </div>
+                        <div class="card-body">
+                            <form action="" method="post">
+                                @csrf
+                                <div class="form-group">
+                                    <label for="">Dari</label>
+                                    <input type="text" name="name" readonly value="{{ Auth::user()->email }}" class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <label for="">Kepada</label>
+                                    <input type="email" name="" id="" class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <label for="">Subject</label>
+                                    <textarea name="" id="" class="form-control" cols="30" rows="10"></textarea>
+                                </div>
+                                <div class="form-group">
+                                    <label for="">File</label>
+                                    <input type="file" name="" id="" class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-block btn-primary">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
 
                     <!-- Main row -->
                     <!-- /.row (main row) -->
